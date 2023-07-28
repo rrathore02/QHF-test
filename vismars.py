b1ef5d1c1b8a7e9a41f396fdfb8d3d3889297585
danielapai <apai@arizona.edu>
2023-07-28 08:04:53 -0700

    Habitat Suitability vs Depth figure added

--- vismars.py
+++ vismars.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def QHFvisualize(screen,sf,Suitability_Distribution,Temperature_Distribution,BondAlbedo_Distribution,GreenHouse_Distribution,Pressure_Distribution,Depth_Distribution, runid):
+def QHFvisualize(screen,sf,Suitability_Distribution,Temperature_Distribution,BondAlbedo_Distribution,GreenHouse_Distribution,Pressure_Distribution,Depth_Distribution, runid,Suitability_Plot,Variable):
     if screen:
         # Scaling factor
         sf = 1.0
@@ -22,6 +22,28 @@ def QHFvisualize(screen,sf,Suitability_Distribution,Temperature_Distribution,Bon
         other_node_color='blue'
         labelcolor='black'
         labeloffset=-0.05
+
+    plt.rcParams.update({'font.size': 4})
+    fig, ax = plt.subplots(figsize=(2.00, 3.00), dpi=400)
+    #ax.facecolor(bkgcolor)
+    #ax.edgecolor(selected_edgecolor)
+    # Add frame:
+    #for zz in np.arange(int(NumProbes)):
+    ax.set_xlim([0.,1.2])
+    ax.set_ylim([1000,-90])
+    #ax.plot(Suitability_Plot[zz],SavedParameters[zz].Depth, marker='o',alpha=0.8,color='lightblue',markersize=3)
+    ax.plot(Suitability_Plot,Variable, alpha=0.8,color='lightblue',markersize=1.1)
+    #ax = G.visualize()
+    #plt.text(0.02,0.02, 'Average Suitability %.2f' % np.mean(Suitability_Distribution),fontsize=4*sf,color=labelcolor,transform=ax.transAxes)
+    ax.plot([-100.,100],[0.,0.], linestyle='--', linewidth=0.5, color='red')
+    ax.text(0.7,40.,'Surface', fontsize=3, color='red')
+    ax.text(0.1,-60.,keyparams.runid, fontsize=3.5)
+    ax.set_title('Habitat Suitability for Mars Subsurface')
+    ax.set_ylabel('Depth [m]')
+    ax.set_xlabel('Probability of Habitat Suitability')
+    fig.tight_layout()
+    plt.show()
+
 
     ax = plt.axes(projection='3d')#,figsize=(4.00, 2.00), dpi=400)
     ax.scatter3D(Temperature_Distribution, Depth_Distribution, Suitability_Distribution, c=Suitability_Distribution, cmap='seismic',s=0.7,alpha=0.5);
