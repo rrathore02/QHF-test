cb7a4196e949d719ca7a1f18728783a8dfbc595f
danielapai <apai@arizona.edu>
2023-08-01 09:37:19 -0700

    Bug fixes

--- liquidwater.py
+++ liquidwater.py
@@ -4,7 +4,7 @@ import keyparams
 import numpy as np
 
 #===========[ Cyanobacteria ]======================
-def Module_LiquidWater_UAv1p0():
+def LiquidWater_UAv1p0():
     global Module_LiquidWater
     ModuleTemp = mcmodules.Module()
     ModuleTemp.define_name('Liquid Water AE v1.0')
@@ -12,25 +12,24 @@ def Module_LiquidWater_UAv1p0():
     ModuleTemp.add_input('Surface_Pressure')
     ModuleTemp.add_output('Suitability')
     def _execute(self):
-        global Surface_Pressure, Surface_Temperature, GSuitability
+        #global Surface_Pressure, Surface_Temperature, GSuitability
         #breakpoint()
-        if keyparams.Surface_Pressure > 0.07 and keyparams.Surface_Temperature < 373. and keyparams.Surface_Temperature > 273.:
+        if keyparams.Pressure > 0.07 and keyparams.Temperature < 373. and keyparams.Temperature > 273.:
             keyparams.Suitability=1.0
         else:
             keyparams.Suitability=0.0
         print('Suitability calculated in module', keyparams.Suitability)
         # Key parameters are returned to the main function, allowing the Visualization
         # of the results. Below a dictionary is defined with the relevant values
-        keyparams.runid = keyparams.runid+' | '+ 'Cyanobacteria AE V1.0'
-        KeyParameters={
-            'Suitability' : keyparams.Suitability,
-            'Surface_Temperature' : np.ndarray.item(keyparams.Surface_Temperature),
-            'Surface_Pressure' : np.ndarray.item(keyparams.Surface_Pressure),
-            'Bond_Albedo' : np.ndarray.item(keyparams.Bond_Albedo),
-            'GreenhouseWarming' : np.ndarray.item(keyparams.GreenhouseWarming),
-            'runid' : keyparams.runid+' | '+ 'Cyanobacteria AE V1.0'
-            }
-        return KeyParameters
+        keyparams.runid = keyparams.runid+' | '+ ModuleTemp.name
+        #KeyParameters={
+        #    'Suitability' : keyparams.Suitability,
+    #        'Surface_Temperature' : np.ndarray.item(keyparams.Surface_Temperature),
+    #        'Surface_Pressure' : np.ndarray.item(keyparams.Surface_Pressure),
+    #        'Bond_Albedo' : np.ndarray.item(keyparams.Bond_Albedo),
+    #        'GreenhouseWarming' : np.ndarray.item(keyparams.GreenhouseWarming),
+    ##        }
+        return #KeyParameters
 
     ModuleTemp.execute = types.MethodType(_execute, ModuleTemp)
     ModuleTemp.activate()
