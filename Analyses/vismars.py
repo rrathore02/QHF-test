0afd4fe31510657ccb9d7a4234f8e61e2c229c0c
rrathore02 <rrathore@arizona.edu>
2025-07-27 20:03:03 -0700

    Update vismars.py

--- Analyses/vismars.py
+++ Analyses/vismars.py
@@ -2,10 +2,9 @@ import keyparams
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from scipy import stats
 
-def sanitize_filename(name):
-    return "".join(c for c in name if c.isalnum() or c in (' ', '_', '-')).rstrip()
-
+#(screen,sf,Suitability_Distribution,Temperature_Distribution,BondAlbedo_Distribution,GreenHouse_Distribution,Pressure_Distribution,runid):
 def QHFvisualize(screen,sf,Suitability_Distribution,Temperature_Distribution,BondAlbedo_Distribution,GreenHouse_Distribution,Pressure_Distribution,Depth_Distribution, runid,Suitability_Plot,Variable,HabitatLogo):
     if screen:
         # Scaling factor
@@ -26,124 +25,105 @@ def QHFvisualize(screen,sf,Suitability_Distribution,Temperature_Distribution,Bon
         labelcolor='black'
         labeloffset=-0.05
 
-    ### ===================================================================================== ##
-    ##   Suitability vs. Depth profile plot
-    ### ===================================================================================== ##
+    ax = plt.axes(projection='3d')#,figsize=(4.00, 2.00), dpi=400)
+    ax.scatter3D(Temperature_Distribution, Pressure_Distribution, Suitability_Distribution, c=Suitability_Distribution, cmap='seismic',s=0.7,alpha=0.5);
+    fig = ax.get_figure()
+    fig.set_size_inches(8, 8)
+    if screen: ax.scatter3D(Temperature_Distribution, Pressure_Distribution, Suitability_Distribution, c=Suitability_Distribution, cmap='seismic',s=4.9,alpha=0.1);
+    ax.set_xlabel('Surface Temperature [K]')
+    #ax.set_xrange(220,450)
+    ax.axes.set_xlim3d(left=220, right=450)
+    ax.set_ylabel('Surface Pressure [bar]')
+    ax.set_zlabel('Habitat Suitability')
+    ax.set_title(keyparams.runid + ' | S = %.2f' % np.mean(Suitability_Distribution),fontsize=10*sf,color=labelcolor)
+    #ax.text(0.02,0.02,0.02, 'Average Suitability %.2f' % np.mean(Suitability_Distribution),fontsize=10*sf,color=labelcolor,transform=ax.transAxes)
 
-    plt.rcParams.update({'font.size': 4})
-    fig, ax = plt.subplots(figsize=(2.00, 4.00), dpi=200)
-    # Suitability vs. negative Depth 
-    #   negative Depth so that it is 'elevation' instead
-    #   above surface = positive, below surface = negative
-    ax.plot(Suitability_Plot,-np.asarray(Variable), alpha=0.8,color='black',markersize=1.1)
-    # Limit suitability axis
-    ax.set_xlim([0.,1.2])
-    # Limit elevation to 5000 m below the surface
-    ax.set_ylim([-15000,2000]) 
-    # Plot shaded regions to indicate above/below the surface
-    #    blue = atmosphere, red = sub-surface
-    ax.text(0.7, 750.,'Atmosphere', fontsize=5, color='gray')
-    ax.fill_between([0.0,1.2],[0_000,0_000],[5000,5000], color='lightblue',alpha=0.3) # make sure these somewhat match axes limits!
-    ax.text(0.7,-5_000.,'Subsurface', fontsize=5, color='red')
-    ax.plot([-100.,100],[0.,0.], linestyle='--', linewidth=0.5, color='red') # dotted line to highlight the surface
-    ax.fill_between([0.0,1.2],[0_000,0_000],[-100_000,-100_000], color='red',alpha=0.05)
-    # Set axes labels and titles
-    ax.set_title('Habitat Suitability for Mars Subsurface \n '+ keyparams.runid)
-    ax.set_ylabel('Elevation [m]')
-    ax.set_xlabel('Probability of Habitat Suitability')
-    # Add logo of the habitat to the plot (optional)
+    # Add logo of the habitat
     im = plt.imread(HabitatLogo)
     newax = fig.add_axes([0.75, 0.55, 0.15, 0.15], anchor='NE')
     newax.set_axis_off()
     newax.imshow(im)
-    # save and show figure
-    fig.tight_layout()
-    fig.savefig('Figures/Mars_HS-Depth.png')
-    fig.savefig('Figures/Mars_HS-Depth.svg') # vector format
-    plt.show()
 
-    ### ======================================================================================= ##
-    ##    3D Suitability vs. Temperature vs. Depth scatter plot
-    ### ======================================================================================= ##
 
-    ax = plt.axes(projection='3d')#,figsize=(4.00, 2.00), dpi=400)
-    ax.scatter3D(Temperature_Distribution, -np.array(Depth_Distribution), Suitability_Distribution, c=Suitability_Distribution, cmap='seismic',s=1.7,alpha=0.3);
-    fig = ax.get_figure()
-    fig.set_size_inches(8, 8)
-    plt.rcParams.update({'font.size': 10})
-    if screen: ax.scatter3D(Temperature_Distribution, -np.array(Depth_Distribution), Suitability_Distribution, c=Suitability_Distribution, cmap='seismic',s=4.9,alpha=0.1,fontsize=10*sf,);
-    ax.set_xlabel('Temperature [K]',fontsize=10*sf)
-    ax.tick_params(labelsize=8*sf)
-    #ax.set_xrange(220,450)
-    #ax.axes.set_xlim3d(left=220, right=650)
-    ax.axes.set_xlim3d(left=min(Temperature_Distribution), right=max(Temperature_Distribution))
-    ax.set_ylabel('Elevation [m]',fontsize=10*sf)
-    ax.axes.set_ylim3d(bottom=0, top=-max(Depth_Distribution))
-    ax.set_zlabel('Habitat Suitability',fontsize=10*sf)
-    ax.set_title(keyparams.runid + ' | S = %.2f' % np.mean(Suitability_Distribution),fontsize=10*sf,color=labelcolor)
-    #ax.text(0.02,0.02,0.02, 'Average Suitability %.2f' % np.mean(Suitability_Distribution),fontsize=10*sf,color=labelcolor,transform=ax.transAxes)
-    # add logo:
-    newax = fig.add_axes([0.75, 0.65, 0.10, 0.10], anchor='NE')
-    newax.set_axis_off()
-    newax.imshow(im)
-    fig.savefig('Figures/Mars_3D-Plot.png')
-    fig.savefig('Figures/Mars_3D-Plot.svg')
+    #fig.tight_layout()
+    fig.savefig('Figures/'+keyparams.runid+'_HS-Pressure-Temperature.png')
+    fig.savefig('Figures/'+keyparams.runid+'_HS-Pressure-Temperature.svg') # vector format
     plt.show()
 
 
     #breakpoint()
 
     # Multi-plot showing distributions of key parameters
-    #sns.reset_orig()
-    N_iter = 10.
-    #fig=plt.figure(figsize=(4.00, 2.00), dpi=400)
-    #fig, axs = plt.subplots(2, 2)
-    #axs[0, 0].hist(Temperature_Distribution,bins=np.clip(math.floor(N_iter/60.), 5, 30),histtype='step')
-    #axs[0, 0].hist(Temperature_Distribution,histtype='step')
-    #axs[0, 0].set_title('Surface Temp. [K]')
-    #axs[0,0].set(xlabel='[K]', ylabel='y-label')
-    #axs[0, 1].hist(Pressure_Distribution, histtype='step')
-    #axs[0, 1].set_title('Surface Pressure [bar]')
-    #axs[1, 0].hist(BondAlbedo_Distribution, histtype='step')
-    #axs[1, 0].set_title('Bond Albedo')
-    #axs[1, 1].hist(GreenHouse_Distribution,histtype='step')
-    #axs[1, 1].set_title('Greenhouse Warming [K]')
-    #fig.tight_layout()
+    # This will need to be optimizable from the module loaded, but now I specify the parameters
+    
+    ## For now, hard-coding what is/isn't a prior!!
+    
+    ## for parameters with a cut-off, the build-up at the cut-off messes up the automatically-drawn axes limits
+    ##    so, define a way to set y-limit based on the height of the distribution at its median
 
 
+    nbins=np.floor(len(Temperature_Distribution)/30.).astype(int)
+    print(nbins)
+    #fig=plt.figure(figsize=(6.00, 2.00), dpi=400)
     fig, axs = plt.subplots(2, 2,constrained_layout=True,figsize=(12.00, 4.00))
-    #N_iter = 100.
-
-    counts, bins = np.histogram(Temperature_Distribution, bins=20)
+    N_iter = 100.
+    #axs[0, 0].hist(Temperature_Distribution) #,bins=np.clip(math.floor(N_iter/60.), 5, 30))
+    
+    # surface temperature is a RESULT
+    counts, bins = np.histogram(Temperature_Distribution, bins=nbins)
+    axs[0, 0].text(0.8, 0.8, 'Result', c='blue', transform=axs[0,0].transAxes, fontsize=12)
     axs[0, 0].stairs(counts, bins, fill=1)
-    axs[0, 0].set_xlabel('Temperature [K]')
+    axs[0, 0].set_xlabel('Surface Temp. [K]')
+    axs[0, 0].set_ylim(0, np.sort(counts)[-2]+10) # set y-limit based on 2nd-highest bin height + buffer to prevent build-ups at cutoff from messing with default y-limit
     #axs[0,0].set(xlabel='[K]', ylabel='y-label')
 
-    counts, bins = np.histogram(Pressure_Distribution, bins=20)
-    axs[0, 1].stairs(counts, bins, fill=1)
+    # surface pressure is a PRIOR
+    # note this "Pressure" is surface pressure, not interior pressure at depth
+    counts, bins = np.histogram(Pressure_Distribution, bins=nbins)
+    axs[0, 1].text(0.8, 0.8, 'Prior', c='red', transform=axs[0,1].transAxes, fontsize=12)
+    axs[0, 1].stairs(counts, bins, fill=1, color='red')
     axs[0, 1].set_xlabel('Surface Pressure [bar]')
+    axs[0, 1].set_ylim(0, np.sort(counts)[-2]+10)
 
-    counts, bins = np.histogram(BondAlbedo_Distribution, bins=20)
-    axs[1, 0].stairs(counts, bins, fill=1)
+    # bond albedo is a prior
+    counts, bins = np.histogram(BondAlbedo_Distribution, bins=nbins)
+    axs[1, 0].text(0.8, 0.8, 'Prior', c='red', transform=axs[1,0].transAxes, fontsize=12)
+    axs[1, 0].stairs(counts, bins, fill=1, color='red')
     axs[1, 0].set_xlabel('Bond Albedo')
+    axs[1, 0].set_ylim(0, np.sort(counts)[-2]+10)
 
-    counts, bins = np.histogram(GreenHouse_Distribution, bins=20)
-    axs[1, 1].stairs(counts, bins, fill=1)
+    # greenhouse distribution is a PRIOR
+    counts, bins = np.histogram(GreenHouse_Distribution, bins=nbins)
+    axs[1, 1].text(0.8, 0.8, 'Prior', c='red', transform=axs[1,1].transAxes, fontsize=12)
+    axs[1, 1].stairs(counts, bins, fill=1, color='red')
     axs[1, 1].set_xlabel('Greenhouse Warming [K]')
+    axs[1, 1].set_ylim(0, np.sort(counts)[-2]+10)
 
     fig.suptitle('Probability Distribution of Key Parameters  '+keyparams.runid, fontsize=13)
 
-    safe_runid = sanitize_filename(keyparams.runid)
-    fig.savefig('Figures/' + safe_runid + '_Multi-plot.png')
-    fig.savefig('Figures/' + safe_runid + '_Multi-plot.svg')
+
+    #axs[0, 0].hist(Temperature_Distribution) #,bins=np.clip(math.floor(N_iter/60.), 5, 30))
+    #axs[0, 0].set_xlabel('Surface Temp. [K]')
+    #axs[0, 1].hist(Pressure_Distribution ) #, bins=np.clip(math.floor(N_iter/60.), 5, 30))
+    #axs[0, 1].set_xlabel('Surface Pressure [bar]')
+    #axs[1, 0].hist(BondAlbedo_Distribution)#, bins=np.clip(math.floor(N_iter/60.), 5, 30))
+    #axs[1, 0].set_xlabel('Bond Albedo')
+    #axs[1, 1].hist(GreenHouse_Distribution )#), bins=np.clip(math.floor(N_iter/60.), 5, 30))
+    #axs[1, 1].set_xlabel('Greenhouse Warming [K]')
+
+
+    fig.tight_layout()
+
+    fig.savefig('Figures/'+keyparams.runid+'_Multi-plot.png')
+    fig.savefig('Figures/'+keyparams.runid+'_Multi-plot.svg')
+
     #for ax in axs.flat:
     #    ax.set(xlabel='x-label', ylabel='y-label')
 
     # Hide x labels and tick labels for top plots and y ticks for right plots.
     #for ax in axs.flat:
     #    ax.label_outer()
-    #plt.show()
-    plt.close()
+    plt.show()
 
 
     return
