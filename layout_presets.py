951ec5fdeb9c78036b49649c6fb9fe1e2463b5c2
rrathore02 <rrathore@arizona.edu>
2025-07-27 19:54:04 -0700

    Add files via upload
    for better graphs

---
+++ layout_presets.py
@@ -0,0 +1,41 @@
+presets = {
+    "europa": {
+        "Equilibrium \n Temperature": (0.00, -0.15),
+        "Semi major axis": (0.00, -0.25),
+        "Leaky Greenhouse": (0.00, 0.20),
+        "Planet\nPrimary\nProperties": (0.00, 0.15)
+    },
+    "mars": {
+        "Equilibrium \n Temperature": (0.05, 0.05),
+        "Semi major axis": (0.00, -0.15),
+        "Leaky Greenhouse": (0.00, 0.10),
+        "Planet\nPrimary\nProperties": (0.00, 0.12),
+        "Methanogens AE v1.0": (0.00, 0.12),
+        "Stellar \n Properties": (-0.12, -0.05),
+        "Albedo Prior": (-0.10, -0.05),
+        "Luminosity": (0.10, 0.10),
+        "Bond Albedo": (0.15, -0.10),
+        "Stellar Mass": (-0.15, 0.10)
+    }
+}
+
+
+
+label_offsets = {
+    "europa": {
+        "Stellar \n Properties": (-0.15, -0.05),
+        "Albedo Prior": (-0.20, -0.05),
+        "Equilibrium \n Temperature": (0.10, 0.05),
+        "Surface \n Pressure Prior": (0.07, -0.05),
+        "Methanogens AE v1.0": (0.00, 0.10),
+        "Planet\nPrimary\nProperties": (0.00, 0.10)
+    },
+    "mars": {
+        "Equilibrium \n Temperature": (-0.12, 0.02),
+        "Planet\nPrimary\nProperties": (0.00, 0.10),
+        "Methanogens AE v1.0": (0.00, 0.10),
+        "Greenhouse Strength": (0.00, 0.10),
+        "Bond Albedo": (0.00, 0.10),
+        "Surface \n Pressure Prior": (0.07, -0.05)
+    }
+}
